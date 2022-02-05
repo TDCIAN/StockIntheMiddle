4bc2a6641cb196787c0573ade8c840c896a2dc8a
TDCIAN <tdcian71@gmail.com>
2022-02-05 23:10:47 +0900

    configure base settings

---
+++ StockIntheMiddle/Controllers/WatchListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockIntheMiddle
+//
+//  Created by JeongminKim on 2022/02/05.
+//
+
+import UIKit
+
+class WatchListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
