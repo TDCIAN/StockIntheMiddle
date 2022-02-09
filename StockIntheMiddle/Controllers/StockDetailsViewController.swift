777267ab1d934479088a6a7bd8506fcbcca4892b
TDCIAN <tdcian71@gmail.com>
2022-02-10 08:49:28 +0900

    configure stockDetailVC

--- StockIntheMiddle/Controllers/StockDetailsViewController.swift
+++ StockIntheMiddle/Controllers/StockDetailsViewController.swift
@@ -14,6 +14,15 @@ class StockDetailsViewController: UIViewController {
     private let companyName: String
     private var candleStickData: [CandleStick]
     
+    private let tableView: UITableView = {
+       let table = UITableView()
+        table.register(NewsHeaderView.self, forHeaderFooterViewReuseIdentifier: NewsHeaderView.identifier)
+        table.register(NewsStoryTableViewCell.self, forCellReuseIdentifier: NewsStoryTableViewCell.identifier)
+        return table
+    }()
+    
+    private var stories: [NewsStory] = []
+    
     // MARK: - Init
     init(symbol: String, companyName: String, candleStickData: [CandleStick] = []) {
         self.symbol = symbol
@@ -32,6 +41,52 @@ class StockDetailsViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        setUpTable()
+        fetchFinancialData()
+        fetchNews()
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    // MARK: - Private
+    private func setUpTable() {
+        view.addSubviews(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func fetchFinancialData() {
+        renderChart()
+    }
+
+    private func fetchNews() {
+        
+    }
+    private func renderChart() {
+        
+    }
+    
+
+}
+
+extension StockDetailsViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension StockDetailsViewController: NewsHeaderViewDelegate {
+    func newsHeaderViewDidTapAddButton(_ headerView: NewsHeaderView) {
+        // Add to watchlist
+    }
 }
