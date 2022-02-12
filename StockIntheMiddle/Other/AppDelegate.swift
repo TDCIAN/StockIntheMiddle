7baa9d1b269d04a7aa6bbc053fe1b13b7ea95fe4
TDCIAN <tdcian71@gmail.com>
2022-02-12 21:48:50 +0900

    remove comment

--- StockIntheMiddle/Other/AppDelegate.swift
+++ StockIntheMiddle/Other/AppDelegate.swift
@@ -10,67 +10,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-
-        debug()
-        
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) { }
 
-    private func debug() {
-//        APICaller.shared.search(query: "Apple") { result in
-//            switch result {
-//            case .success(let response):
-//                print("AppDelegate - response: \(response.result)")
-//            case .failure(let error):
-//                print("AppDelegate - error: \(error)")
-//            }
-//        }
-        
-//        APICaller.shared.news(for: .topStories) { result in
-//            switch result {
-//            case .success(let news):
-//                print("AppDeleagte - news: \(news)")
-//            case .failure(let error):
-//                print("AppDeleagte - error: \(error)")
-//            }
-//        }
-        
-//        APICaller.shared.news(for: .company(symbol: "MSFT")) { result in
-//            switch result {
-//            case .success(let news):
-//                print("AppDeleagte - news: \(news)")
-//            case .failure(let error):
-//                print("AppDeleagte - error: \(error)")
-//            }
-//        }
-        
-//        APICaller.shared.marketData(for: "AAPL", numberOfDays: 1) { result in
-//            switch result {
-//            case .success(let data):
-//                let candleSticks = data.candleSticks
-//                print("AppDelegate - marketData: \(candleSticks)")
-//            case .failure(let error):
-//                print("AppDeleagte - error: \(error)")
-//            }
-//        }
-    }
 }
 
