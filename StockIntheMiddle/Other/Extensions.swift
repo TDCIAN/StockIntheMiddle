679b66712098cc6adf9b0e40d526cb7330ccfc68
TDCIAN <tdcian71@gmail.com>
2022-02-12 14:53:07 +0900

    change date format

--- StockIntheMiddle/Other/Extensions.swift
+++ StockIntheMiddle/Other/Extensions.swift
@@ -52,7 +52,7 @@ extension UIImageView {
 extension String {
     static func string(from timeInterval: TimeInterval) -> String {
         let date = Date(timeIntervalSince1970: timeInterval)
-        return DateFormatter.prettyDateFormatter.string(from: date)
+        return DateFormatter.newsDateFormatter.string(from: date)
     }
     
     static func percentage(from double: Double) -> String {
