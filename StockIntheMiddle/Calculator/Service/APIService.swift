816b6c8f2784b50b72edd49066a23e2fe8077232
TDCIAN <tdcian71@gmail.com>
2022-02-24 17:31:29 +0900

    add service

---
+++ StockIntheMiddle/Calculator/Service/APIService.swift
@@ -0,0 +1,94 @@
+//
+//  APIService.swift
+//  StockIntheMiddle
+//
+//  Created by JeongminKim on 2022/02/24.
+//
+
+import Foundation
+import Combine
+
+struct APIService {
+    
+    enum APIServiceError: Error {
+        case encoding
+        case badRequest
+    }
+    
+    static var key: String {
+        let keys: [String] = ["PX40A1II4YND2OGE", "5GIMV9VKZDVHLJTQ", "9ENLS3NPR86NE79F"]
+        return keys.randomElement() ?? "PX40A1II4YND2OGE"
+    }
+    
+    // use combine
+    func fetchSymbolsPublisher(keywords: String) -> AnyPublisher<CalcSearchResults, Error> {
+        
+        let result = parseQuery(text: keywords)
+        var symbol = ""
+        switch result {
+        case .success(let query):
+            symbol = query
+        case .failure(let error):
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+        
+        let urlString = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=\(symbol)&apikey=\(APIService.key)"
+        
+        let urlResult = parseURL(urlString: urlString)
+        
+        switch urlResult {
+        case .success(let url):
+            return URLSession.shared.dataTaskPublisher(for: url)
+                .map({ $0.data })
+                .decode(type: CalcSearchResults.self, decoder: JSONDecoder())
+                .receive(on: RunLoop.main)
+                .eraseToAnyPublisher()
+        case .failure(let error):
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+    }
+    
+    func fetchTimeSeriesMonthlyAdjustedPublisher(keywords: String) -> AnyPublisher<TimeSeriesMonthlyAdjusted, Error> {
+        
+        let result = parseQuery(text: keywords)
+        var symbol = ""
+        switch result {
+        case .success(let query):
+            symbol = query
+        case .failure(let error):
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+        
+        let urlString = "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=\(symbol)&apikey=\(APIService.key)"
+        
+        let urlResult = parseURL(urlString: urlString)
+        
+        switch urlResult {
+        case .success(let url):
+            return URLSession.shared.dataTaskPublisher(for: url)
+                .map({ $0.data })
+                .decode(type: TimeSeriesMonthlyAdjusted.self, decoder: JSONDecoder())
+                .receive(on: RunLoop.main)
+                .eraseToAnyPublisher()
+        case .failure(let error):
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+    }
+    
+    private func parseQuery(text: String) -> Result<String, Error> {
+        if let query = text.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
+            return .success(query)
+        } else {
+            return .failure(APIServiceError.encoding)
+        }
+        
+    }
+    
+    private func parseURL(urlString: String) -> Result<URL, Error> {
+        if let url = URL(string: urlString) {
+            return .success(url)
+        } else {
+            return .failure(APIServiceError.badRequest)
+        }
+    }
+}
---
+++ StockIntheMiddle/Calculator/Service/DCAService.swift
@@ -0,0 +1,110 @@
+//
+//  DCAService.swift
+//  StockIntheMiddle
+//
+//  Created by JeongminKim on 2022/02/24.
+//
+
+import Foundation
+
+struct DCAService {
+    func calculate(asset: Asset, initialInvestmentAmount: Double, monthlyDollorCostAverageAmount: Double, initialDateOfInvestmentIndex: Int) -> DCAResult {
+        
+        let investmentAmount = getInvestmentAmount(initialInvestmentAmount: initialInvestmentAmount,
+                                                   monthlyDollorCostAverageAmount: monthlyDollorCostAverageAmount,
+                                                   initialDateOfInvestmentIndex: initialDateOfInvestmentIndex)
+        
+        let latestSharePrice = getLatestSharePrice(asset: asset)
+        
+        let numberOfShares = getNumberOfShares(asset: asset,
+                                               initialInvestmentAmount: initialInvestmentAmount,
+                                               monthlyDollarCostAveragingAmount: monthlyDollorCostAverageAmount,
+                                               initialDateOfInvestmentIndex: initialDateOfInvestmentIndex)
+        
+        let currentValue = getCurrentValue(numberOfShares: numberOfShares, latestSharePrice: latestSharePrice)
+        
+        let isProfitable = currentValue > investmentAmount
+        
+        let gain = currentValue - investmentAmount
+        
+        /*
+         example
+         investmentAmount: $10,000
+         (1)
+         currentValue: $12,000
+         gain = +$2000
+         yield = $2000 / $10,000 = 20%
+         (2)
+         currentValue: $7,000
+         gain: -$3,000
+         yield: -$3,000 / $10,000 = -30%
+         */
+        
+        let yield = gain / investmentAmount
+        
+        let annualReturn = getAnnualReturn(currentValue: currentValue,
+                                           investmentAmount: investmentAmount,
+                                           initialDateOfInvestmentIndex: initialDateOfInvestmentIndex)
+        
+        return .init(currentValue: currentValue,
+                     investmentAmount: investmentAmount,
+                     gain: gain,
+                     yield: yield,
+                     annualReturn: annualReturn,
+                     isProfitable: isProfitable)
+        
+        // currentValue = numberOfShares (initial + DCA) * latest share price
+    }
+    
+    func getInvestmentAmount(initialInvestmentAmount: Double,
+                                     monthlyDollorCostAverageAmount: Double,
+                                     initialDateOfInvestmentIndex: Int) -> Double {
+        var totalAmount = Double()
+        totalAmount += initialInvestmentAmount
+        let dollarCostAveragingAmounts = initialDateOfInvestmentIndex.doubleValue * monthlyDollorCostAverageAmount
+        totalAmount += dollarCostAveragingAmounts
+        return totalAmount
+    }
+    
+    private func getAnnualReturn(currentValue: Double, investmentAmount: Double, initialDateOfInvestmentIndex: Int) -> Double {
+        let rate = currentValue / investmentAmount
+        let years = (initialDateOfInvestmentIndex.doubleValue + 1) / 12
+        let result = pow(rate, (1 / years)) - 1
+        return result
+    }
+    
+    private func getCurrentValue(numberOfShares: Double, latestSharePrice: Double) -> Double {
+        return numberOfShares * latestSharePrice
+    }
+    
+    private func getLatestSharePrice(asset: Asset) -> Double {
+        return asset.timeSeriesMonthlyAdjusted.getMonthInfos().first?.adjustedClose ?? 0
+    }
+    
+    private func getNumberOfShares(asset: Asset,
+                                   initialInvestmentAmount: Double,
+                                   monthlyDollarCostAveragingAmount: Double,
+                                   initialDateOfInvestmentIndex: Int) -> Double {
+        
+        var totalShares = Double()
+        
+        let initialInvestmentOpenPrice = asset.timeSeriesMonthlyAdjusted.getMonthInfos()[initialDateOfInvestmentIndex].adjustedOpen
+        let initialInvestmentShares = initialInvestmentAmount / initialInvestmentOpenPrice
+        totalShares += initialInvestmentShares
+        asset.timeSeriesMonthlyAdjusted.getMonthInfos().prefix(initialDateOfInvestmentIndex).forEach { monthInfo in
+            let dcaInvestmentShares = monthlyDollarCostAveragingAmount / monthInfo.adjustedOpen
+            
+            totalShares += dcaInvestmentShares
+        }
+        return totalShares
+    }
+}
+
+struct DCAResult {
+    let currentValue: Double
+    let investmentAmount: Double
+    let gain: Double
+    let yield: Double
+    let annualReturn: Double
+    let isProfitable: Bool
+}
